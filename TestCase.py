c72b4f8fb5f518196484ed5df85dfb3bf0f1c383
GaneshManal <ganesh.manal@nutanix.com>
2015-08-14 15:03:40 +0530

    introduced error

--- TestCase.py
+++ TestCase.py
@@ -9,6 +9,7 @@ class TestStringMethods(unittest.TestCase):
 
     def test_upper(self):
         self.assertEqual('foo'.upper(), 'FOO')
+        time and tide
 
     def test_isupper(self):
         self.assertTrue('FOO'.isupper())
