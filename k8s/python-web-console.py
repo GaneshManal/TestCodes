43e620e90a210fd2acd25c981162b1b277466d6c
ganeshmanal <gmanal@cisco.com>
2022-06-11 19:08:54 +0530

    cleanup

--- k8s/python-web-console.py
+++
@@ -1,7 +0,0 @@
-from code import InteractiveConsole
-console = InteractiveConsole()
-
-inp = True
-while inp:
-    inp = input('>>> ')
-    console.runcode(inp)
--- test-terraform-esx.py
+++
@@ -1,7 +0,0 @@
-from views.stack import StackHandler
-if __name__ == "__main__":
-    stack_obj = StackHandler()
-    stack_obj.test()
-
-
-
